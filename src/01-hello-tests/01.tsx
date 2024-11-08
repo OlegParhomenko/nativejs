d216bcfb8c0901e531f3b8569fbc76dfa9deea8e
Oleg Parhomenko <shocked194@gmail.com>
2024-11-08 15:03:18 +0300

    Intro and example for unit-tests

---
+++ src/01-hello-tests/01.tsx
@@ -0,0 +1,16 @@
+export function splitIntoWords(sentense: string) {
+    const words = sentense.toLowerCase().split(' ')
+    return words.filter(word => word !== '' && word !== '-')
+        .map(word => word
+            .replace('!','')
+            .replace('.','')
+            .replace(',',''));
+}
+
+export function sum(a: number, b: number) {
+    return a + b;
+}
+
+export function mult(a: number, b: number) {
+    return a * b;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {splitIntoWords} from "./01-hello-tests/01";
+
+const sentense = 'Hello my friends!';
+
+
 
 function App() {
   return (
